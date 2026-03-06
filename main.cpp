b479a168ee009dc13cfd49be5964d76c7e70f994
Shashank <rokshashank@gmail.com>
2026-03-06 20:20:20 +0530

    Implementing Luhn Algorithm

--- main.cpp
+++ main.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
 #include <string>
 using namespace std;
+bool luhncheck(string cnm){{
+    int sum=0;
+    bool dbldigit = false;
+    for(int i = cnm.length() - 1;i>=0;i--){
+        int digit = cnm[i] - '0';
+        if(dbldigit){
+            digit *= 2;
+            if(digit > 9){
+                digit -= 9;
+            }
+        }
+        sum += digit;
+        dbldigit  = !dbldigit;
+    }
+    return sum %10 == 0;
+}}
 int main()
 {
     string cardnumber;
     cout<<"-----Credit Card Validator-----"<<endl;
     cout<<"Enter Your Credit Card Number: ";
     cin>>cardnumber;
-    cout<<"User Input : "<<cardnumber;
+    if(luhncheck(cardnumber)){
+        cout<<"VALID CREDIT CARD"<<endl;
+    }
+    else{
+        cout<<"INVALID CREDIT CARD"<<endl;
+    }
     return 0;
 }
