607b06fe9de4d63adc9db1d627777f2c51ceb963
Shashank <rokshashank@gmail.com>
2026-03-09 23:05:16 +0530

    Cardtype Function File

---
+++ src/cardtype.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+struct crule{
+    string name;
+    vector <string> prefixes;
+};
+void cardtype(string cnm){
+    vector<crule>rule = 
+    {
+        {"Visa",{"4"}},
+        {"Mastercard",{"51","52","53","54","55"}},
+        {"American Express",{"34","37"}}
+    };
+    for(const auto &rl : rule){
+        for(const string &s1 : rl.prefixes){
+            
+        }
+    }
+
+}
+int main(){
+
+}
