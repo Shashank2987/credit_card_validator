6c976df6c64908f874141596a9ff5978074aed8e
Shashank <rokshashank@gmail.com>
2026-03-25 19:51:29 +0530

    database update

--- src/cardtype.cpp
+++ src/cardtype.cpp
@@ -2,32 +2,42 @@
 #include <string>
 #include <vector>
 using namespace std;
-struct crule{
+
+struct crule {
     string name;
-    vector <string> prefixes;
+    vector<string> prefixes;
 };
-void cardtype(string cnm){
-    vector<crule>rule = 
-    {
-        {"Visa",{"4"}},
-        {"Mastercard",{"51","52","53","54","55"}},
-        {"American Express",{"34","37"}},
-        {"Discover", {"6011","65"}},
-        {"JCB", {"35"}},
-        {"Diners Club", {"300","301","302","303","304","305","36","38"}},
-        {"RuPay", {"60","65","81","82"}},
-        {"UnionPay", {"62"}},
-        {"Maestro", {"50","56","57","58","63","67"}}
-    };
-    for(const auto &rl : rule){
-        for(const string &s1 : rl.prefixes){
-            if(cnm.substr(0,s1.length()) == s1){
-                cout<<"Card Type : "<<rl.name<<endl;
+
+void cardtype(const string &cnm) {
+    vector<crule> rule;
+rule.push_back({"Visa", {"4"}});
+rule.push_back({"Mastercard", {"51","52","53","54","55"}});
+rule.push_back({"American Express", {"34","37"}});
+rule.push_back({"Discover", {"6011","65"}});
+rule.push_back({"JCB", {"35"}});
+rule.push_back({"Diners Club", {"300","301","302","303","304","305","36","38"}});
+rule.push_back({"RuPay", {"60","65","81","82"}});
+rule.push_back({"UnionPay", {"62"}});
+rule.push_back({"Maestro", {"50","56","57","58","63","67"}});
+
+    for (const auto &rl : rule) {
+        for (const auto &s1 : rl.prefixes) {
+            if (cnm.substr(0, s1.length()) == s1) {
+                cout << "Card Type: " << rl.name << endl;
+                return;
             }
         }
     }
 
+    cout << "Unknown Card Type" << endl;
 }
-int main(){
 
+int main() {
+    string card;
+    cout << "Enter card number: ";
+    cin >> card;
+
+    cardtype(card);
+
+    return 0;
 }
